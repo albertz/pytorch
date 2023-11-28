0850c706232da13cbbcf61f100817b3b6c98ef48
Jon Chuang <9093549+jon-chuang@users.noreply.github.com>
2023-11-27 19:49:56 -0500

    [AOT Refactor] functional utils
    ghstack-source-id: 17e0e3a466dc7ccb1c7c376f8be4432c2c2501a1
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/114552

--- torch/_functorch/aot_autograd.py
+++ torch/_functorch/aot_autograd.py
@@ -31,7 +31,7 @@ from torch._subclasses.functional_tensor import FunctionalTensor, FunctionalTens
 from torch.fx import Interpreter
 from torch.fx.experimental.proxy_tensor import is_sym_node
 from torch.fx.experimental.symbolic_shapes import (
-    ShapeEnv, is_concrete_int, fx_placeholder_vals, definitely_true, definitely_false, sym_eq
+    ShapeEnv, is_concrete_int, fx_placeholder_vals, definitely_false, sym_eq
 )
 from torch.multiprocessing.reductions import StorageWeakRef
 from torch.nn.utils import stateless
@@ -67,6 +67,19 @@ from ._aot_autograd.logging_utils import (  # noqa: F401
     setup_stacktrace_preservation_hooks,
     describe_input,
     format_guard_bug_msg,
+)
+from ._aot_autograd.functional_utils import (  # noqa: F401
+    is_fun,
+    to_fun,
+    from_fun,
+    sync_functional_tensor,
+    has_metadata_mutation,
+    are_all_mutations_hidden_from_autograd,
+    gen_alias_from_base,
+    has_same_metadata,
+    was_tensor_updated,
+    was_tensor_metadata_updated,
+    assert_functional_graph,
 )
 
 zip = strict_zip
@@ -717,192 +730,6 @@ class TensorAlias:
     alias: torch.Tensor
 
 
-def has_same_metadata(t1, t2):
-    return (
-        definitely_true(sym_eq(t1.size(), t2.size()))
-        and definitely_true(sym_eq(t1.stride(), t2.stride()))
-        and definitely_true(t1.storage_offset() == t2.storage_offset())
-        and t1.is_conj() == t2.is_conj()
-        and t1.is_neg() == t2.is_neg()
-    )
-
-
-def gen_alias_from_base(aliased_base_tensor, target_meta_tensor, target_requires_grad):
-    # Try to do view-replay if possible.
-    # fall back to .as_strided() if we can't.
-    if target_meta_tensor._base is not None:
-        # The base that we want to replay our view off of might have a different shape than the view's original base.
-        b = target_meta_tensor._base
-        abt = aliased_base_tensor
-        # Don't unnecessarily call as_strided if nothing changed; as_strided's
-        # backward is poorly implemented and slow
-        if abt is not b and (
-            abt.size() != b.size() or
-            abt.stride() != b.stride() or
-            abt.storage_offset() != b.storage_offset()
-        ):
-            reshaped_base_tensor = aliased_base_tensor.as_strided(
-                b.size(), b.stride(), b.storage_offset()
-            )
-        else:
-            reshaped_base_tensor = aliased_base_tensor
-        out = target_meta_tensor._view_func(reshaped_base_tensor)
-        # This shape mismatch can happen due to a bug in inplace/view handling in autograd.
-        # Try putting a breakpoint here and running
-        # `test/functorch/test_aotdispatch TestAOTAutograd.test_output_all_alias_types`
-        # Also, https://github.com/pytorch/pytorch/issues/49825
-        #
-        # As a stopgap, we'll fall back to as_strided.
-        if out is not None and out.shape == target_meta_tensor.shape:
-            if aliased_base_tensor.requires_grad and not target_requires_grad:
-                out = out.detach()
-            elif not aliased_base_tensor.requires_grad and target_requires_grad:
-                out.requires_grad_(True)
-            return out
-    size = target_meta_tensor.size()
-    stride = target_meta_tensor.stride()
-    storage_offset = target_meta_tensor.storage_offset()
-    if aliased_base_tensor.is_complex() and not target_meta_tensor.is_complex():
-        aliased_out = torch.view_as_real(aliased_base_tensor).as_strided(
-            size, stride, storage_offset
-        )
-    elif not aliased_base_tensor.is_complex() and target_meta_tensor.is_complex():
-        aliased_out = torch.view_as_complex(aliased_base_tensor).as_strided(
-            size, stride, storage_offset
-        )
-    else:
-        aliased_out = aliased_base_tensor.as_strided(size, stride, storage_offset)
-    # For outputs aliasing inputs, we need to check if the requires-gradness has changed.
-    if aliased_base_tensor.requires_grad and not target_requires_grad:
-        aliased_out = aliased_out.detach()
-    elif not aliased_base_tensor.requires_grad and target_requires_grad:
-        aliased_out.requires_grad_(True)
-    # For outputs aliasing inputs, we need to check if the dtype has changed.
-    # as_strided() is the "most generic" view, but it does not cover cross-dtype views
-    if aliased_out.dtype != target_meta_tensor.dtype:
-        aliased_out = aliased_out.view(target_meta_tensor.dtype)
-    return aliased_out
-
-def to_fun(t):
-    if isinstance(t, Tensor):
-        if is_traceable_wrapper_subclass(t):
-            # See Note [Functionalization always runs last]
-            # This means that if we want to "functionalize" a subclass, we need to ensure that the functional wrapper
-            # goes at the bottom.
-            # recurse here, so we can support nested wrapper subclasses
-            out = transform_subclass(t, lambda _, inner_t: to_fun(inner_t))
-            torch._mirror_autograd_meta_to(t, out)
-            return out
-        else:
-            return FunctionalTensor.to_functional(t)
-    else:
-        return t
-
-def sync_functional_tensor(t):
-    if is_traceable_wrapper_subclass(t):
-        attrs, ctx = t.__tensor_flatten__()
-        for attr in attrs:
-            sync_functional_tensor(getattr(t, attr))
-    else:
-        torch._sync(t)
-
-# When subclasses are involved, t here will usually look something like:
-# SubclassA(SubclassB(FunctionalTensor(_to_functional_tensor(FakeTensor))))
-def from_fun(t):
-    if isinstance(t, Tensor) and is_traceable_wrapper_subclass(t):
-        # See Note [Functionalization always runs last]
-        # This means that if we want to "functionalize" a subclass, we need to ensure that the functional wrapper
-        # goes at the bottom.
-        # recurse here, so we can support nested wrapper subclasses
-        out = transform_subclass(t, lambda _, inner_t: from_fun(inner_t))
-        torch._mirror_autograd_meta_to(t, out)
-        return out
-
-    if not isinstance(t, FunctionalTensor):
-        # quick sanity assert
-        if isinstance(t, torch.Tensor):
-            assert not torch._is_functional_tensor(t)
-        return t
-    sync_functional_tensor(t)
-    return torch._from_functional_tensor(t.elem)
-
-def is_fun(t):
-    if isinstance(t, Tensor) and is_traceable_wrapper_subclass(t):
-        # See Note [Functionalization always runs last]
-        # This means that if we want to "functionalize" a subclass, we need to ensure that the functional wrapper
-        # goes at the bottom.
-        # recurse here, so we can support nested wrapper subclasses
-        t_attrs, _ = t.__tensor_flatten__()
-        t_inners = [getattr(t, attr) for attr in t_attrs]
-        any_fun = any(is_fun(x) for x in t_inners)
-        all_fun = all(is_fun(x) for x in t_inners)
-        assert any_fun == all_fun
-        return any_fun
-
-    return isinstance(t, FunctionalTensor)
-
-# t here is either
-# (1) A FunctionalTensor(_to_functional_tensor(FakeTensor))
-# (2) A traceable tensor subclass that holds a FunctionalTensor
-def has_metadata_mutation(t):
-    if is_traceable_wrapper_subclass(t):
-        attrs, _ = t.__tensor_flatten__()
-        # A tensor subclass was updated if any of its inner elements were updated
-        return any(has_metadata_mutation(getattr(t, attr)) for attr in attrs)
-    else:
-        assert isinstance(t, FunctionalTensor)
-        return torch._functionalize_has_metadata_mutation(t.elem)
-
-def are_all_mutations_hidden_from_autograd(t):
-    if is_traceable_wrapper_subclass(t):
-        attrs, _ = t.__tensor_flatten__()
-        # If all inner elements are mutations hidden from autograd, then it is a mutation hidden from autograd.
-        return all(are_all_mutations_hidden_from_autograd(getattr(t, attr)) for attr in attrs)
-    else:
-        assert isinstance(t, FunctionalTensor)
-        return torch._functionalize_are_all_mutations_hidden_from_autograd(t.elem)
-
-# new_arg and arg here are either:
-# (1) both a FakeTensor
-# (2) both a traceable tensor subclass that holds a FakeTensor
-# Pre-condition: the two args are the "old" and "new" inputs from running functionalization.
-# When we run functionalization and wrap our inputs into FunctionalTensors,
-# we can detect whether or not an input was mutated by checking to see if the inner tensor has changed
-#
-# Normally it would be enough just to check if arg is new_arg, which is normally enough for functionalization
-# to confirm that inputs were not mutated when running the user's model with functionalization on.
-# But when we have subclass inputs, we can't rely on that:
-# `from_fun(to_fun(x)) is x` will return False, because the call to `from_fun` constructs
-# a brand new subclass instance: we are calling __tensor_unflatten__, and going
-# from Subclass(FakeTensor) to Subclass(FunctionalTensor(FakeTensor))
-def was_updated(arg, new_arg):
-    if is_traceable_wrapper_subclass(arg):
-        assert is_traceable_wrapper_subclass(new_arg)
-        attrs, _ = arg.__tensor_flatten__()
-        new_attrs, _ = new_arg.__tensor_flatten__()
-        assert attrs == new_attrs
-        # A tensor subclass was updated if any of its inner elements were updated
-        return any(was_updated(getattr(arg, attr), getattr(new_arg, attr)) for attr in attrs)
-    else:
-        return arg is not new_arg
-
-# new_arg and arg here are either:
-# (1) both a FakeTensor
-# (2) both a traceable tensor subclass that holds a FakeTensor
-# Pre-condition: the two args are the "old" and "new" inputs from running functionalization.
-# When we run functionalization and wrap our inputs into FunctionalTensors,
-# we can detect whether or not an input was mutated by checking to see if the inner tensor has changed,
-# but shares storage with the old input
-def was_metadata_updated(arg, new_arg):
-    if is_traceable_wrapper_subclass(arg):
-        assert is_traceable_wrapper_subclass(new_arg)
-        attrs, _ = arg.__tensor_flatten__()
-        new_attrs, _ = new_arg.__tensor_flatten__()
-        assert attrs == new_attrs
-        # A tensor subclass was updated if any of its inner elements were updated
-        return any(was_metadata_updated(getattr(arg, attr), getattr(new_arg, attr)) for attr in attrs)
-    else:
-        return arg is not new_arg and StorageWeakRef(arg.untyped_storage()) == StorageWeakRef(new_arg.untyped_storage())
 
 def requires_subclass_dispatch(args, fw_metadata: ViewAndMutationMeta) -> bool:
     args_flattened = pytree.arg_tree_leaves(*args)
@@ -1011,8 +838,8 @@ def run_functionalized_fw_and_collect_metadata(
                 new_arg = arg
             else:
                 new_arg = from_fun(f_arg)
-            if was_updated(arg, new_arg):
-                if was_metadata_updated(arg, new_arg):
+            if was_tensor_updated(arg, new_arg):
+                if was_tensor_metadata_updated(arg, new_arg):
                     mutates_data = False
                     mutates_metadata = True
                 else:
@@ -1933,29 +1760,6 @@ def aot_dispatch_base(flat_fn, flat_args: List[Tensor], aot_config: AOTConfig, *
     )
 
     return compiled_fn
-
-
-# Returns the number of detected copy_
-def assert_functional_graph(fx_g: torch.fx.Graph, *, allow_input_mutations: bool = False) -> int:
-    placeholders = set()
-    copy_count = 0
-    # NB: It would also be nice to verify that the mutations all happen at the
-    # end, but we also do some administrative views after mutations so this
-    # isn't actually true.  (TODO: Could this cause problems for Inductor?)
-    for n in fx_g.nodes:
-        if n.op == "placeholder":
-            placeholders.add(n)
-        if isinstance(n.target, torch._ops.OpOverload):
-            if n.target is aten.copy_.default and allow_input_mutations:
-                suffix = True
-                # Can only copy_ into an input, and can only do so once
-                assert n.args[0] in placeholders
-                placeholders.remove(n.args[0])
-                copy_count += 1
-            else:
-                assert not n.target._schema.is_mutable, \
-                    f'aot_autograd expected to have an entirely functional graph, but found {n.format_node()}'
-    return copy_count
 
 
 def are_differentiable_views(view1, view2):
